3850d3e4d47e4a300f93ad9e3c2ae7192b46f0d0
Madan Sapkota <contact@madansapkota.com>
2026-09-08 17:11:24 +0545

    Re-export algorithm policy and limits

---
+++ src/policy/index.ts
@@ -0,0 +1,4 @@
+export { AlgorithmPolicy, decideAlgorithm } from './algorithms.ts';
+export type { AlgorithmDecision, AlgorithmOperation } from './algorithms.ts';
+export { LIMITS_V1, lowerLimits } from './limits.ts';
+export type { Limits } from './limits.ts';
