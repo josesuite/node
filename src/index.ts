b88a32b8fc7c23487552f707a67623254db5436e
Madan Sapkota <contact@madansapkota.com>
2026-09-08 17:11:40 +0545

    Add public package entry point

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { getCapabilityReport } from './capabilities.ts';
+export type { CapabilityReport } from './capabilities.ts';
+export * from './errors/index.ts';
+export * from './jws/index.ts';
+export * from './jwe/index.ts';
+export * from './jwt/index.ts';
+export * from './jwk/index.ts';
+export * from './policy/index.ts';
