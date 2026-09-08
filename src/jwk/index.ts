cbf0b38c9364410267cf7f59446a7a8e19d0a328
Madan Sapkota <contact@madansapkota.com>
2026-09-08 17:06:07 +0545

    Re-export JWK snapshot and key container APIs

---
+++ src/jwk/index.ts
@@ -0,0 +1,10 @@
+export { decryptKeyContainer } from './decrypt.ts';
+export type { DecryptKeyContainerOptions, DecryptKeyContainerResult, EncryptedKeyContainerType } from './decrypt.ts';
+export { exportEcPublicJwk, exportOctPublicJwk, exportOkpPublicJwk, exportRsaPublicJwk } from './export.ts';
+export type { ExportMetadata, PublicJwk } from './export.ts';
+export { buildSnapshot, buildSnapshotBytes, distinctPrincipals, readJwksEntries } from './jwks.ts';
+export type { KeySnapshot, SnapshotBinding, SnapshotEntry, SnapshotInput, SnapshotResult } from './jwks.ts';
+export { computeThumbprint, parseThumbprintUri, toThumbprintUri } from './thumbprint.ts';
+export type { ThumbprintResult } from './thumbprint.ts';
+export { importKeyBytes } from '../key/import.ts';
+export type { ImportOptions, ImportResult, UsableKey } from '../key/import.ts';
