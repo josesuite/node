51dcbbdbdc003787bc2216f61a6eea8cce883c15
Madan Sapkota <contact@madansapkota.com>
2026-09-08 15:26:06 +0545

    Re-export error categories and error types

---
+++ src/errors/index.ts
@@ -0,0 +1,4 @@
+export { ERROR_CATEGORIES, TRUST_STAGES, isErrorCategory } from './codes.ts';
+export type { ErrorCategory, TrustStage } from './codes.ts';
+export { JoseError, isJoseError } from './error.ts';
+export type { JoseDiagnostic, JoseErrorDetail, JoseErrorOptions } from './error.ts';
